113954597479b48bb63a6ea08f98bb529e9e5157
Nahyun Kim <hedghog1004@naver.com>
2024-03-07 23:49:30 +0900

    🎨 Design: 아이콘 colorFilter 설정 및 디테일 수정
    Related to: #3

--- Toss/app/src/main/java/com/compose/toss/ui/HomeScreen.kt
+++ Toss/app/src/main/java/com/compose/toss/ui/HomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -22,12 +23,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.compose.toss.R
+import com.compose.toss.data.Assets
+import com.compose.toss.data.assets
 
 @Composable
 fun HomeScreen() {
@@ -62,16 +66,20 @@ private fun TossHomeTopBar(modifier: Modifier = Modifier) {
         ) {
             Image(
                 painter = painterResource(R.drawable.ic_place),
-                contentDescription = "",
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
                 modifier = Modifier
-                    .size(24.dp)
+                    .size(32.dp)
+                    .padding(3.dp)
                     .offset(x = (-20).dp)
             )
             Image(
                 painter = painterResource(R.drawable.ic_alert),
-                contentDescription = "",
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
                 modifier = Modifier
-                    .size(24.dp)
+                    .size(32.dp)
+                    .padding(3.dp)
             )
         }
     }
@@ -88,6 +96,7 @@ fun TossLogo(
     ) {
         Image(
             painter = painterResource(R.drawable.logo_toss_symbol),
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
             contentDescription = stringResource(R.string.app_name),
             modifier = modifier
                 .size(30.dp)
@@ -95,6 +104,7 @@ fun TossLogo(
         Image(
             painter = painterResource(R.drawable.logo_toss_text),
             contentDescription = stringResource(R.string.app_name),
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
             modifier = modifier
                 .width(60.dp)
                 .height(30.dp)
@@ -113,7 +123,7 @@ private fun TossBankContainer(
             contentColor = MaterialTheme.colorScheme.onPrimaryContainer
         ),
         modifier = modifier
-            .fillMaxSize()
+            .fillMaxWidth()
             .offset(y = 16.dp)
     ) {
         Row(
@@ -121,7 +131,7 @@ private fun TossBankContainer(
             verticalAlignment = Alignment.CenterVertically,
             modifier = modifier
                 .padding(horizontal = 22.dp, vertical = 20.dp)
-                .fillMaxSize()
+                .fillMaxWidth()
         ) {
             Text(
                 text = "토스뱅크",
@@ -131,11 +141,11 @@ private fun TossBankContainer(
             )
             Image(
                 painter = painterResource(R.drawable.ic_arrow_right),
-                contentDescription = "토스뱅크",
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurface),
                 modifier = modifier
                     .height(16.dp)
                     .width(16.dp)
-                    .background(Color.White)
             )
         }
     }
